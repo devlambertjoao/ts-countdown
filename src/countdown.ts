474ba7cc139ea66db419f9978f3b8ea93399b002
Joao Lambert <devlambertjoao@gmail.com>
2023-02-14 16:00:58 -0300

    Testing the creating class

--- src/countdown.ts
+++ src/countdown.ts
@@ -1,3 +1,15 @@
 export default class Countdown {
-  constructor() {}
+  hours: number;
+  minutes: number;
+  seconds: number;
+
+  constructor(
+    hours: number = 0,
+    minutes: number = 25, 
+    seconds: number = 0
+  ) {
+    this.hours = hours;
+    this.minutes = minutes;
+    this.seconds = seconds;
+  }
 }
--- test/countdown.test.ts
+++ test/countdown.test.ts
@@ -1,7 +1,17 @@
 import Countdown from "../src/countdown";
 
-test('nothing', () => {
+test('can create with default hours, minutes, and seconds', () => {
   const countdown = new Countdown();
   expect(countdown).not.toBe(null);
+  expect(countdown.hours).toBe(0);
+  expect(countdown.minutes).toBe(25);
+  expect(countdown.seconds).toBe(0);
 })
 
+test('can create with hours, minutes and seconds', () => {
+  const countdown = new Countdown(1, 20, 20);
+  expect(countdown).not.toBe(null);
+  expect(countdown.hours).toBe(1);
+  expect(countdown.minutes).toBe(20);
+  expect(countdown.seconds).toBe(20);
+})
